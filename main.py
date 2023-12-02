9c074cdbaf0c8715150b9309d172ba0fae169c5d
Andrei Danelli <danelliandrei@gmail.com>
2023-12-02 19:57:56 -0300

    feat: Adicionado imagens e realizado ajuste no fonte

--- main.py
+++ main.py
@@ -1,47 +1,81 @@
 import os.path
-
 import numpy as np
 import cv2
+
 from pip._internal.cli.spinners import open_spinner
 
-img = cv2.imread('image/image.jpg')
-Z = img.reshape((-1,3))
+import constants.const
+
+
+def saveImageResultDirectory():
+    cv2.imwrite(directory, res2)
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+
+
+def showConfgAllImageOrigin():
+    heigth_origin, width_origin, chanel_origin = img.shape
+    count = validatorColorUniqueImageOrigin()
+
+    print('\n')
+    print(f'Info Image Origin')
+    print(f'Dimension: {heigth_origin}x{width_origin}')
+    print(f'Heigth: {heigth_origin} pixels')
+    print(f'Witdh: {width_origin} pixels')
+    print(f'Size Image: {os.path.getsize(constants.const.PATH_IMAGE_ORIGIN) / 1024:.0f} Kb')
+    print(f'Color Unique: {count}')
+
+
+def showConfgAllImagemResult():
+    heigth_result, width_result, chanel_result = res2.shape
+    count = validatorColorUniqueImageResult()
+
+    print('\n')
+    print(f'Info Image Result')
+    print(f'Dimension: {heigth_result}x{width_result}')
+    print(f'Heigth: {heigth_result} pixels')
+    print(f'Witdh: {width_result} pixels')
+    print(f'Size Image: {os.path.getsize(directory) / 1024:.0f} Kb')
+    print(f'Color Unique: {count}')
+
+
+def validatorColorUniqueImageOrigin():
+    image_rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+    pixels = image_rgb.reshape((-1, 3))
+    color_unique = np.unique(pixels, axis=0)
+    return len(color_unique)
+
+
+def validatorColorUniqueImageResult():
+    img_result = cv2.imread(directory)
+    image_rgb = cv2.cvtColor(img_result, cv2.COLOR_BGR2RGB)
+    pixels = image_rgb.reshape((-1, 3))
+    color_unique = np.unique(pixels, axis=0)
+    return len(color_unique)
+
+
+img = cv2.imread(constants.const.PATH_IMAGE_ORIGIN)
+Z = img.reshape((-1, 3))
+K = 1000
 
 # convert to np.float32
 Z = np.float32(Z)
 
 # define criteria, number of clusters(K) and apply kmeans()
 criteria = (cv2.TERM_CRITERIA_EPS + cv2.TERM_CRITERIA_MAX_ITER, 10, 1.0)
-K = 1
-ret,label,center=cv2.kmeans(Z,K,None,criteria,10,cv2.KMEANS_RANDOM_CENTERS)
+ret, label, center = cv2.kmeans(Z, K, None, criteria, 10, cv2.KMEANS_RANDOM_CENTERS)
 
 # Now convert back into uint8, and make original image
 center = np.uint8(center)
 res = center[label.flatten()]
-res2 = res.reshape((img.shape))
+res2 = res.reshape(img.shape)
 
-# cv2.imshow('res2',res2)
-directory = os.path.join('image-result', 'output_image.jpg')
-cv2.imwrite(directory, res2)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+print(f'Save Image Result in Directory')
+directory = os.path.join(constants.const.PATH_IMAGE_RESULT, constants.const.NAME_IMAGE_RESULT+'_K'+str(K)+'.png')
 
-# info imagem original
-heigth_origin, width_origin, chanel_origin = img.shape
-size_origin = os.path.getsize('image/image.jpg')/1024
+saveImageResultDirectory()
 
-print('\n')
-print(f'Info Image Origin')
-print(f'Heigth: {heigth_origin} pixels')
-print(f'Witdh: {width_origin} pixels')
-print(f'Size Image: {size_origin:.0f}')
-print('\n')
+showConfgAllImageOrigin()
 
-# info imagem result
-heigth_result, width_result, chanel_result = res2.shape
-size_result = os.path.getsize(directory)/1024
+showConfgAllImagemResult()
 
-print(f'Info Image Result')
-print(f'Heigth: {heigth_result} pixels')
-print(f'Witdh: {width_result} pixels')
-print(f'Size Image: {size_result:.0f}')
